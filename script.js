024407eb8d4f8a953bc65b01b46c8b5be2092e39
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-03-22 19:28:42 +0300

    Create script.js
    +
    -
    *
    /

---
+++ script.js
@@ -0,0 +1,23 @@
+var num1 = '23'
+var num2 = '22'
+
+console.log('+', num1 + num2)
+console.log('-', num1 - num2)
+console.log('*', num1 * num2)
+console.log('/', num1 / num2)
+
+
+
+
+var str1 = 'hello'
+var str2 = 'World'
+
+console.log( str1 +' '+ str2)
+const peir = '111'
+const peir2 = 111
+const peir3 = 'cool'
+
+console.log (peir + ' ' + peir3 + ' ' + peir2 )
+
+console.log ( true + 1 )
+console.log ( false + 1 )
